cbb8dd19a1d871fc80283f604e356f023111586f
neil0212 <wonderwall.wong@gmail.com>
2016-10-08 01:21:34 +0000

    login

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ def entries(page=1):
     page_index = page - 1
 
     count = session.query(Entry).count()
-
+  
     start = page_index * PAGINATE_BY
     end = start + PAGINATE_BY
 
@@ -28,7 +28,8 @@ def entries(page=1):
         has_next=has_next,
         has_prev=has_prev,
         page=page,
-        total_pages=total_pages
+        total_pages=total_pages,
+      
     )
     
 
@@ -85,3 +86,50 @@ def delete_entry_post(id):
     return redirect(url_for("entries"))
     
     
+
+@app.route("/<int:page>limit20")
+def entries_page(page=1):
+    page_index = page - 1
+    count = session.query(Entry).count()
+    paginate_by = page
+    PAGINATE_BY=paginate_by 
+    start = page_index * PAGINATE_BY
+    end = start + PAGINATE_BY
+
+    total_pages = (count - 1) // PAGINATE_BY + 1
+    has_next = page_index < total_pages - 1
+    has_prev = page_index > 0
+
+    entries = session.query(Entry)
+    entries = entries.order_by(Entry.datetime.desc())
+    entries = entries[start:end]
+
+    return render_template("entries.html",
+        entries=entries,
+        has_next=has_next,
+        has_prev=has_prev,
+        page=page,
+        total_pages=total_pages
+    )
+
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+    
+    
+from flask import flash
+from flask_login import login_user
+from werkzeug.security import check_password_hash
+from .database import User
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("entries"))
